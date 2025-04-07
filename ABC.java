0b00b3b464c1bd8101ce459a6c7c0125363e013d
Sudhir Tiwari <sudhiropera@gmail.com>
2025-04-07 12:31:45 +0530

    Add Xyz and modify ABC.java

--- ABC.java
+++ ABC.java
@@ -3,6 +3,7 @@ class ABC
 
 public static void main(String[] args){
 	System.out.println("Sudhir");
+	System.out.println("new line added");
 }
 
 }
---
+++ Xyz.java
@@ -0,0 +1,10 @@
+public class Xyx
+{
+
+	private int x;
+	private char c;
+
+	public int getX(){
+		return x;
+	}
+}
